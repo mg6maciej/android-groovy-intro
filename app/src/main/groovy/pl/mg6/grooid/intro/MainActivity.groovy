cc6d495138fefc079a4393c0766a135f1400a001
Maciej Górski <maciek.gorski@gmail.com>
2015-05-21 08:12:54 +0200

    Add compile static annotation.

--- app/src/main/groovy/pl/mg6/grooid/intro/MainActivity.groovy
+++ app/src/main/groovy/pl/mg6/grooid/intro/MainActivity.groovy
@@ -1,6 +1,8 @@
 package pl.mg6.grooid.intro
 
 import android.support.v7.app.AppCompatActivity
+import groovy.transform.CompileStatic
 
+@CompileStatic
 final class MainActivity extends AppCompatActivity {
 }
